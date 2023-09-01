0de6efce9c3b0a5a60690c4b76bb7482ed1c6865
Taimoor Khan <82448357+taimooorr@users.noreply.github.com>
2023-09-01 14:26:36 +0500

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect,useState } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+/* Thired Party Imports */
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+/* Custom Import files */
+import LoginScreen from './Screen/LoginScreen';
+import RegisterScreen from './Screen/RegisterScreen';
+import HomeScreen from './Screen/HomeScreen';
+import AddChatScreen from './Screen/AddChatScreen';
+import ChatScreen from './Screen/ChatScreen';
+import { auth, onAuthStateChangedAuthUser } from './firebase';
+const Stack = createNativeStackNavigator();
+
+const globalScreenOptions = {
+  headerStyle: { backgroundColor: "#2C6BED" },
+  headerTitleStyle: {
+    color: "white",
+  },
+  headerTintColor: "white",
+  headerTitleAlign: "center",
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function App() {
+  const [initialRouteName, setInitialRouteName] = useState("Login");
+  useEffect(() => {
+    onAuthStateChangedAuthUser((authUser) => {
+      if (authUser) {
+     
+      }
+    });
+    
+  }, [initialRouteName])
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home" screenOptions={globalScreenOptions}>
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Register" component={RegisterScreen} />
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="AddChat" component={AddChatScreen} />
+        <Stack.Screen name="Chat" component={ChatScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
